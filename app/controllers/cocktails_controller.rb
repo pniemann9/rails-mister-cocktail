3b9d2698a3ecda5f312bbccdb37f75144a5e78e3
Philippe Niemann <pniemann@orange.fr>
2020-05-15 15:15:36 +0200

    My rails app before be push on heroku

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,7 +6,6 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
-    @review = Review.new
   end
 
   def new
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -11,9 +11,11 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      @review = Review.new
       render "cocktails/show"
     end
+  end
+
+  def new
   end
 
   def destroy
